eadee521ba66355f07fa1b4953bfd532354a54c9
lourdama <Mattys.Lourdault@etu.univ-smb.fr>
2025-06-05 15:07:58 +0200

    Classes de connexion à la BDD + interface d'édition de la BDD

---
+++ PilotApp/Model/ICrud.cs
@@ -0,0 +1,20 @@
+
+
+namespace TD3_BindingBDPension.Model
+{
+    public interface ICrud<T>
+   {
+        public int Create();
+
+        public void Read();
+
+        public int Update();
+
+        public int Delete();
+      
+        public  List<T> FindAll();
+
+        public List<T> FindBySelection(string criteres);
+   
+   }
+}
