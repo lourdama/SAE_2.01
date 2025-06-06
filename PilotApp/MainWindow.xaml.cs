d1426193f4b977c267095e038a4a37cc66fd8884
Raphael Coltelli <Raphael.Coltelli@etu.univ-smb.fr>
2025-06-06 10:55:12 +0200

    page connexion

--- PilotApp/MainWindow.xaml.cs
+++ PilotApp/MainWindow.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System.Text;
+﻿using PilotApp.View;
+using System.Reflection.Metadata;
+using System.Text;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -19,6 +21,7 @@ namespace PilotApp
         public MainWindow()
         {
             InitializeComponent();
+            pageVisible.Content = new PageConnexion();
         }
     }
 }
---
+++ PilotApp/View/PageConnexion.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace PilotApp.View
+{
+    /// <summary>
+    /// Logique d'interaction pour PageConnexion.xaml
+    /// </summary>
+    public partial class PageConnexion : UserControl
+    {
+        public PageConnexion()
+        {
+            InitializeComponent();
+        }
+
+        private void connexion_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show($"Login : {login.Text} Mot de passe : {mdp.Text}");
+        }
+    }
+}
