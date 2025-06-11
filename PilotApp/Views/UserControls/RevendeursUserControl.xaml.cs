48937a39ac0a89edbd034cbad2015f812c007c2b
lourdama <mattys.lourdault@etu.univ-smb.fr>
2025-06-11 18:50:10 +0200

    Ajouter Revendeur

--- PilotApp/Views/UserControls/RevendeursUserControl.xaml.cs
+++ PilotApp/Views/UserControls/RevendeursUserControl.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using PilotApp.Models;
 
 namespace PilotApp.Views.UserControls
 {
@@ -20,10 +21,40 @@ namespace PilotApp.Views.UserControls
     /// </summary>
     public partial class RevendeursUserControl : UserControl
     {
+        public AjouterRevendeurUserControl aruc;
         public RevendeursUserControl()
         {
             InitializeComponent();
             this.DataContext = MainWindow.Instance.Pilot.LesRevendeurs;
+        }
+
+        private void butAjouter_Click(object sender, RoutedEventArgs e)
+        {
+            Revendeur nouveauRevendeur = new Revendeur();
+            AjouterRevendeurUserControl ajouterRevendeur = new AjouterRevendeurUserControl(this, nouveauRevendeur, Action.Creer);
+            ajouterRevendeur.ValidationFaite += OnValidationFaiteAjouter;
+            this.aruc = ajouterRevendeur;
+            MainWindow.Instance.vueActuelle.Content = this.aruc;
+        }
+
+        private void OnValidationFaiteAjouter(bool estValide)
+        {
+            if (estValide)
+            {
+                try
+                {
+                    this.aruc.UnRevendeur.Id = this.aruc.UnRevendeur.Create();
+                    MainWindow.Instance.Pilot.LesRevendeurs.Add(this.aruc.UnRevendeur);
+                }
+                catch
+                {
+                    MessageBox.Show("Erreur lors de l'ajout du revendeur.");
+                }
+            }
+            else
+            {
+                MessageBox.Show("Validation annulée.");
+            }
         }
     }
 }
