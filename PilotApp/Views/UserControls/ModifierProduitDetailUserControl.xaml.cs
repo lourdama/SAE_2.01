ab8bd55f9d419c9ac0ed065dc9fc83346244d5ee
Illyasse Labrahimi <Ilyiasse.Labrahimi@etu.univ-smb.fr>
2025-06-11 19:43:57 +0200

    .

--- PilotApp/Views/UserControls/ModifierProduitDetailUserControl.xaml.cs
+++ PilotApp/Views/UserControls/ModifierProduitDetailUserControl.xaml.cs
@@ -23,6 +23,7 @@ namespace PilotApp.Views.UserControls
     /// </summary>
     public partial class ModifierProduitDetailUserControl : UserControl
     {
+        public AjouterProduitCommandeUserControl apcuc ;
         public Commande Commande { get; }
         public UserControl pagePrec;
         public ModifierProduitDetailUserControl(UserControl pagePrec,Commande commande)
@@ -43,12 +44,59 @@ namespace PilotApp.Views.UserControls
             MainWindow.Instance.vueActuelle.Content = new CommandesUserControl();
         }
 
-        private void butAnnuler_Click(object sender, RoutedEventArgs e)
+
+
+        private void butAjouter_Click(object sender, RoutedEventArgs e)
         {
-            MainWindow.Instance.vueActuelle.Content = new CommandesUserControl();
-            
+            //AjouterProduitCommande fenetre = new AjouterProduitCommande();
+            //fenetre.Owner = this;
+
+            //bool? result = fenetre.ShowDialog();
+            AjouterProduitCommandeUserControl ajouterProduitCommande = new AjouterProduitCommandeUserControl(this);
+            ajouterProduitCommande.ValidationFaite += OnValidationFaiteAjouter;
+            this.apcuc = ajouterProduitCommande;
+            MainWindow.Instance.vueActuelle.Content = this.apcuc;
         }
 
 
+        private void OnValidationFaiteAjouter(bool estValide)
+        {
+            if (estValide)
+            {
+                Produit p = this.apcuc.ProduitSelectionne;
+                decimal[] data = new decimal[] { this.apcuc.Quantite, this.apcuc.Prix * this.apcuc.Quantite };
+
+                // Récupérer la commande via le ViewModel
+                var vm = (ModifierProduitDetailUserControl)this.DataContext;
+                var commande = vm.Commande;
+
+                // Ajouter le produit s'il n'existe pas encore
+                if (!commande.LesSousCommandes.ContainsKey(p))
+                {
+                    commande.LesSousCommandes[p] = data;
+                    MessageBox.Show("Produit ajouté !");
+                }
+                else
+                {
+                    MessageBox.Show("Ce produit est déjà dans la commande.");
+                }
+
+                if (!commande.LesSousCommandes.ContainsKey(p))
+                {
+                    commande.LesSousCommandes[p] = data;
+
+                    MessageBox.Show("Produit ajouté !");
+                }
+
+
+            }
+
+        }
+
+        private void butRetour_Click(object sender, RoutedEventArgs e)
+        {
+            MainWindow.Instance.vueActuelle.Content = this.pagePrec;
+
+        }
     }
 }
